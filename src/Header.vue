419ec62b3b05970f3df822ccbccc8275a3b0ac77
aram2010 <98265596+aram2010@users.noreply.github.com>
2025-10-28 05:18:32 +0000

    Create header and import it

---
+++ src/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <html>
+    <head>
+        <title>Main Page</title>
+    </head>
+    <body>
+        <header>
+            <img src="" alt="FLC Logo" />
+            <h1>FreeLearnCode</h1>
+        </header>
+    </body>
+    </html>
+</template>
+
+<script></script>
+
+<style scoped>
+header {
+    display: flex;
+    background-color: rgb(38, 225, 0);
+    height: 60px;
+}
+</style> 
